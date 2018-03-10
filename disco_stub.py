664bb5bc6f5b51df12d5510c09017ef9ceee7eaa
Paul Kogel <pkogel@me.com>
2018-03-10 12:03:37 +0100

    Simple segment serving.

--- disco_stub.py
+++ disco_stub.py
@@ -1,6 +1,13 @@
+import time
+
+import math
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
+
+times = [3.000979, 3.000000, 3.000000, 3.000000, 3.000000, 3.000000, 3.000000, 2.741417]
+seq_no = 0
+ref_time = -1
 
 
 @app.route('/')
@@ -8,19 +15,48 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/api/reset')
+def api_reset():
+    global seq_no, ref_time
+    seq_no = 0
+    ref_time = -1
+    return ''
+
+
 @app.route('/api/get_tracks')
 def api_endpoint():
-    sample_file = "{"
-    sample_file += '"reference": "1500000",'
-    sample_file += '"segments": ['
+    global ref_time, seq_no
+    if ref_time == -1:
+        ref_time = time.time()
 
-    for i in range(0, 3):
-        sample_file += '{"no": ' + str(i) + ','
-        sample_file += '"duration": 2.0004,'
-        sample_file += '"url": "http://test/location"}'
-        if i < 2:
-            sample_file += ','
-    sample_file += ']}'
+    passed = time.time() - ref_time
+    if passed >= 2:
+        seq_no += 1
+
+    if seq_no >= len(times):
+        sample_file = '{'
+        sample_file += '"segments": []'
+        sample_file += '}'
+    else:
+        segment_offset = 0
+        for i in range(0, seq_no):
+            segment_offset += times[i]
+
+        m_segment_time = math.floor((ref_time + segment_offset) * 1000)
+
+        sample_file = "{"
+        sample_file += '"reference": "' + str(m_segment_time) + '",'
+        sample_file += '"segments": ['
+
+        max = seq_no + 3 if seq_no + 3 < len(times) else len(times)
+
+        for i in range(seq_no, max):
+            sample_file += '{"no": ' + str(i) + ','
+            sample_file += '"duration": ' + "{0:.3f}".format(times[i]) + ' ,'
+            sample_file += '"url": "static/media/out00' + str(i) + '.wav"}'
+            if i < max - 1:
+                sample_file += ','
+        sample_file += ']}'
 
     return Response(sample_file, mimetype='application/json')
 
